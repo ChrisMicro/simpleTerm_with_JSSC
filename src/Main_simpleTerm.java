6ee879fec7fc831063ab21b2ba6e6acb31ac9225
chris <christoph@roboterclub-freiburg.de>
2016-05-23 21:19:47 +0200

    now this program can send characters from the keyboard

--- src/Main_simpleTerm.java
+++ src/Main_simpleTerm.java
@@ -1,12 +1,20 @@
+import java.awt.Color;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.text.Caret;
+import javax.swing.text.DefaultCaret;
 
 import jssc.SerialPort;
 import jssc.SerialPortEvent;
@@ -14,64 +22,19 @@ import jssc.SerialPortEventListener;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
 
-public class Main_simpleTerm 
+public class Main_simpleTerm implements KeyListener
 {
 	String portName = "/dev/ttyACM0";
   
 	JFrame fenster;
-	int textDimension_x=80;
-	int textDimension_y=40;
+	int textDimension_x=60;
+	int textDimension_y=25;
 	boolean SerialPortOpenedFlag=false;
 
 	JTextArea TextBereich;
-    
-	static SerialPort serialPort;
-  
-	public void openSerialConnection()
-	{
-		TextBereich.append("trying to open port\n\r");
-		TextBereich.append(portName+"\n\r");
-		TextBereich.append("\n\r");
-		    
-		serialPort = new SerialPort(portName);
-		try 
-		{
-		  
-		    System.out.println("port open :" + serialPort.openPort());//Open port
-		    serialPort.setParams(SerialPort.BAUDRATE_115200,
-		            SerialPort.DATABITS_8,
-		            SerialPort.STOPBITS_1,
-		            SerialPort.PARITY_NONE);
-		
-		    int mask = SerialPort.MASK_RXCHAR;//Prepare mask
-		    serialPort.setEventsMask(mask);//Set mask
-		    serialPort.addEventListener(new SerialPortReader(TextBereich));//Add SerialPortEventListener
-		    SerialPortOpenedFlag=true;
-		   
-		} catch (SerialPortException ex) 
-		{
-		    TextBereich.append("port error");
-		    System.out.println(ex);
-		}
-	 }
-    
-	public void closeSerialConnection()
-	{
-		try 
-		{
-			if(SerialPortOpenedFlag)
-			{
-				serialPort.closePort();		
-			    SerialPortOpenedFlag=false;
-			}
 
-		} catch (SerialPortException e) 
-		{
-			System.out.println("closePort error");
-			e.printStackTrace();
-		}
-	}
-	    
+	static SerialPort serialPort;
+  	    
 	public Main_simpleTerm()
 	{
 		fenster=new JFrame("java serial terminal");
@@ -86,18 +49,36 @@ public class Main_simpleTerm
 			      }        
 			}
 		);
-	
+
 		//************************ create window with text **********************************
 		JPanel lowerPanel = new JPanel();
 		
 		fenster.getContentPane().add(lowerPanel, "South");
 		
 		TextBereich = new JTextArea(textDimension_y, textDimension_x);
+		TextBereich.setForeground(Color.GREEN);
+		TextBereich.setBackground(Color.BLACK);
 		
 		TextBereich.setLineWrap(true);
 		TextBereich.setWrapStyleWord(true);
+		TextBereich.setEditable(false);
 		lowerPanel.add(new JScrollPane(TextBereich));
-		  
+		
+		TextBereich.addKeyListener(this);
+		
+		/*// cursor 
+        Caret caret = new DefaultCaret()
+        {
+            public void focusGained(FocusEvent e)
+            {
+                setVisible(true);
+                setSelectionVisible(true);
+            }
+        };
+        caret.setVisible(true);        
+        caret.setBlinkRate( UIManager.getInt("TextField.caretBlinkRate") );
+        TextBereich.setCaret(caret);
+		*/  
 		fenster.pack();
 		fenster.setVisible(true);
 	      
@@ -134,10 +115,62 @@ public class Main_simpleTerm
 		}
 	
 	}
-  
+
+	public void openSerialConnection()
+	{
+		TextBereich.append("trying to open port\n\r");
+		TextBereich.append(portName+"\n\r");
+		    
+		serialPort = new SerialPort(portName);
+		try 
+		{
+		  
+		    System.out.println("port open :" + serialPort.openPort());//Open port
+		    serialPort.setParams(SerialPort.BAUDRATE_115200,
+		            SerialPort.DATABITS_8,
+		            SerialPort.STOPBITS_1,
+		            SerialPort.PARITY_NONE);
+		    
+		    // clear rx data
+		    serialPort.purgePort(SerialPort.PURGE_RXCLEAR);
+
+		    
+		    TextBereich.append("BAUDRATE_115200\n\r");
+			TextBereich.append("\n\r");
+		    
+		    int mask = SerialPort.MASK_RXCHAR;//Prepare mask
+		    serialPort.setEventsMask(mask);//Set mask
+		    serialPort.addEventListener(new SerialPortReader(TextBereich));//Add SerialPortEventListener
+		    SerialPortOpenedFlag=true;
+		   
+		} catch (SerialPortException ex) 
+		{
+		    TextBereich.append("port error");
+		    System.out.println(ex);
+		}
+	 }
+    
+	public void closeSerialConnection()
+	{
+		try 
+		{
+			if(SerialPortOpenedFlag)
+			{
+				serialPort.closePort();		
+			    SerialPortOpenedFlag=false;
+			}
+
+		} catch (SerialPortException e) 
+		{
+			System.out.println("closePort error");
+			e.printStackTrace();
+		}
+	}
+
+
 	static class SerialPortReader implements SerialPortEventListener
 	{
-		int NumberOfLinesUntilTextCleared=40;
+		int NumberOfLinesUntilTextCleared=25;
 		JTextArea outputTextBereich;
 
 		public SerialPortReader(JTextArea outputTextBereich)
@@ -165,7 +198,7 @@ public class Main_simpleTerm
 	        if (event.isRXCHAR()) 
 	        {
 	          int numberOfBytes=event.getEventValue();
-	            if (numberOfBytes > 1) 
+	            if (numberOfBytes > 0) 
 	            {
 	                try 
 	                {
@@ -185,7 +218,30 @@ public class Main_simpleTerm
 	    }
 	}
 
+	@Override
+    public void keyTyped(KeyEvent e) {}
+	
+	@Override
+    public void keyPressed(KeyEvent e) 
+    {
+        System.out.println("Taste: " + e.getKeyChar() + ", Code: " + e.getKeyCode()); 
+
+        try 
+        {
+        	char c = e.getKeyChar();
+        	if(SerialPortOpenedFlag && e.getKeyCode()!=16)    	serialPort.writeString(""+c);
+		} catch (SerialPortException e1) 
+		{
+			e1.printStackTrace();
+		}
+    }
     
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	public static void main(String[] args) 
 	{
 		new Main_simpleTerm();
@@ -193,7 +249,7 @@ public class Main_simpleTerm
 
 }
 
-/*************************************************
+/****************************************************
 
 //Arduino Testsender-Code
 
@@ -210,4 +266,22 @@ void loop()
 	delay(50);        
 }
 
+****************************************************/
+/****************************************************
+//Arduino Serial Echo Code
+   
+ 
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("arduino serial echo ready");
+}
+
+void loop() 
+{
+  if (Serial.available()) 
+  {
+    Serial.write(Serial.read());
+  }        
+}
 ****************************************************/
